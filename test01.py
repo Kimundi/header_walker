1de35700fa4647e28775cdaef72c7e110a6278ca
Marvin Löbel <loebel.marvin@gmail.com>
2018-11-13 14:47:30 +0100

    more refactors

--- test01.py
+++ test01.py
@@ -95,13 +95,24 @@ def is_filtered_out(config, path, is_system_file):
             return True
     return False
 
+def search(path, search_paths, base_search_paths):
+    child = None
+    for search_path in search_paths:
+        #print("  Search in", search_path)
+        combined = Path(search_path) / Path(path)
+        #print("  Check ", combined)
+        if combined.is_file():
+            #print("  Found!")
+            child = (str(combined), (search_path in base_search_paths))
+            break
+    return child
 
 def walk_include_tree(sourcepath, source_property, cache, config, search_paths, base_search_paths):
-    parent_tree = source_property["children"]
+    children = source_property["children"]
     is_system_file = source_property["is_in_system_search_path"]
     (quote_paths, bracket_paths) = search_paths
 
-    def local_print_warning(msg):
+    def filtered_print_warning(msg):
         if not is_filtered_out(config, sourcepath, is_system_file):
             print_warning(sourcepath + ": " + msg)
 
@@ -115,21 +126,9 @@ def walk_include_tree(sourcepath, source_property, cache, config, search_paths, 
             elif groups.group(5):
                 includes.append(("bracket", groups.group(5), groups.group(0)))
             else:
-                local_print_warning("Could not parse '" + groups.group(0) + "'")
+                filtered_print_warning("Could not parse '" + groups.group(0) + "'")
 
     #pprint(includes)
-
-    def search(path, search_paths):
-        child = None
-        for search_path in search_paths:
-            #print("  Search in", search_path)
-            combined = Path(search_path) / Path(path)
-            #print("  Check ", combined)
-            if combined.is_file():
-                #print("  Found!")
-                child = (str(combined), (search_path in base_search_paths))
-                break
-        return child
 
     def search_quoted(path):
         child = None
@@ -137,13 +136,10 @@ def walk_include_tree(sourcepath, source_property, cache, config, search_paths, 
         #print("Try {} relative to {}: {}".format(path, sourcepath, path_relative_to_sourcepath))
         if path_relative_to_sourcepath.is_file():
             child = (str(path_relative_to_sourcepath), is_system_file)
-
         if not child:
-            child = search(path, quote_paths)
-
+            child = search(path, quote_paths, base_search_paths)
         if not child:
-            child = search(path, bracket_paths)
-
+            child = search(path, bracket_paths, base_search_paths)
         return child
 
     for (kind, path, span) in includes:
@@ -151,22 +147,22 @@ def walk_include_tree(sourcepath, source_property, cache, config, search_paths, 
         child = None
 
         if kind == "bracket":
-            child = search(path, bracket_paths)
+            child = search(path, bracket_paths, base_search_paths)
         elif kind == "quoted":
             child = search_quoted(path)
         else:
             print_warning("BUG! This should not be reached")
 
         if not child:
-            local_print_warning("Could not resolve " + span)
-        elif child[0] in parent_tree:
-            local_print_warning("Skipping duplicate " + span)
+            filtered_print_warning("Could not resolve " + span)
+        elif child[0] in children:
+            filtered_print_warning("Skipping duplicate " + span)
         elif child[0] in cache:
-            parent_tree[child[0]] = cache[child[0]]
+            children[child[0]] = cache[child[0]]
         else:
             cache[child[0]] = { "is_in_system_search_path": child[1], "children": {} }
-            parent_tree[child[0]] = cache[child[0]]
-            walk_include_tree(child[0], parent_tree[child[0]], cache, config, search_paths, base_search_paths)
+            children[child[0]] = cache[child[0]]
+            walk_include_tree(child[0], children[child[0]], cache, config, search_paths, base_search_paths)
 
 def print_dep_tree(tree, config, indent = "", print_cache = set()):
     for e in tree:
@@ -192,16 +188,16 @@ def run(config, compiler_cmd):
     db = open_json(config["db_file"])
     process_db(db)
 
-    # Iterate over all source filefrom the db, and gather their include trees
+    # Iterate over all source files from the db, and gather their include trees
     trees = []
     walk_cache = {}
     # FIXME: The same file can appear multiple times in the db (for some reason)
     for db_entry in sorted(db, key = lambda x : x["file"]):
         search_paths = scan_compiler_paths(compiler_cmd, db_entry["directory"], db_entry["includes"])
-        f = db_entry["file"]
-        tree = { db_entry["file"] : { "is_in_system_search_path": False, "children": {} } }
-        walk_include_tree(f, tree[f], walk_cache, config, search_paths, base_search_paths)
-        trees.append(tree)
+        filename = db_entry["file"]
+        properties = { "is_in_system_search_path": False, "children": {} }
+        walk_include_tree(filename, properties, walk_cache, config, search_paths, base_search_paths)
+        trees.append({ filename : properties })
 
     if config["print_header_dependencies"]:
         print("Include Tree")
